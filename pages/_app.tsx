38c424868e0166a8cecdcf6c98b23cdced0f5ef3
adamrajch <arajchwald@gmail.com>
2022-09-20 05:01:26 -0500

    almighty push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,42 @@
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { useState } from "react";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
   return (
     <>
       <Head>
-        <title>Page title</title>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <title>Impact React</title>
+
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "dark",
-        }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Component {...pageProps} />
-      </MantineProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: colorScheme,
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,80 @@
-import { Button, Group } from "@mantine/core";
+import { Box, Button, createStyles, Group, Stack } from "@mantine/core";
+import { useState } from "react";
+import ColorToggle from "../components/ColorToggle";
 
+const useStyles = createStyles((theme, _params, getRef) => ({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      flexDirection: "column",
+    },
+  },
+  btn: {
+    width: "140px",
+    height: "40px",
+    border: "2px solid black",
+    borderColor: theme.colorScheme === "dark" ? "orange" : "black",
+    color: theme.colorScheme === "dark" ? "orange" : "black",
+  },
+
+  box: {
+    height: "500px",
+    width: "100%",
+    background:
+      theme.colorScheme === "dark"
+        ? theme.fn.linearGradient(45, "green", "lime")
+        : theme.fn.linearGradient(45, "teal", "lightblue"),
+  },
+  content: {
+    background:
+      theme.colorScheme === "dark"
+        ? theme.fn.linearGradient(45, "orange", "red")
+        : theme.fn.linearGradient(45, "orange", "pink"),
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: theme.colorScheme === "dark" ? "black" : "black",
+  },
+}));
 export default function IndexPage() {
+  const { classes, cx } = useStyles();
+  const [swap, setSwap] = useState<boolean>(false);
   return (
-    <Group mt={50} position="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <Stack>
+      <Group mx="lg" noWrap>
+        <Button
+          variant="outline"
+          className={classes.btn}
+          onClick={() => setSwap(!swap)}
+        >
+          BUTTON
+        </Button>
+        <ColorToggle />
+      </Group>
+      <Box mt={20} className={classes.container}>
+        {swap ? (
+          <>
+            <Box className={classes.box}> </Box>{" "}
+            <Box className={cx(classes.box, classes.content)}>
+              <p style={{ width: "200px" }}>
+                Put this text in the absolute center of the square and make it
+                200px wide!
+              </p>
+            </Box>
+          </>
+        ) : (
+          <>
+            <Box className={cx(classes.box, classes.content)}>
+              <p style={{ width: "200px" }}>
+                Put this text in the absolute center of the square and make it
+                200px wide!
+              </p>
+            </Box>
+            <Box className={classes.box}> </Box>
+          </>
+        )}
+      </Box>
+    </Stack>
   );
 }
